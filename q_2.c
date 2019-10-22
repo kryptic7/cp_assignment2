6234a2882a06c2eb6afb73f933cc11bc02f53166
kryptic7 <56120695+kryptic7@users.noreply.github.com>
2019-10-22 12:14:05 +0530

    Update q_2.c

--- q_2.c
+++ q_2.c
@@ -3,21 +3,21 @@ void main()
 {
   int i,j;
   int space=0;
-  for(i=1;i<=5;i++)
+  for(i=1;i<=5;i++)/*Run parent loop*/
   {
-  	for(j=5;j>=space;j--)
+  	for(j=5;j>=space;j--)/*Prints Space*/
 	{
 		printf(" ");
 	}
-	for(j=1;j<=i;j++)
+	for(j=1;j<=i;j++)/*Run loop to print first part of row*/
 	{
 		printf("%d",j);
 	}
-	for(j=i-1;j>=1;j--)
+	for(j=i-1;j>=1;j--)/*Run loop to print second part of row*/
 	{
 		printf("%d",j);
 	}
 	printf("\n");
-	space++;
+	space++;/*Increments the value of the variable space*/
   }
 }
