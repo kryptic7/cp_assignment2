552dde3f515e02cd2a4a6349360ed7d2a2b6108a
kryptic7 <56120695+kryptic7@users.noreply.github.com>
2019-10-22 12:20:39 +0530

    Update q_1.c

--- q_1.c
+++ q_1.c
@@ -2,13 +2,13 @@
 void main()
 {
 	int i,j,k;
-	for(i=1;i<=5;i++)
+	for(i=1;i<=5;i++)/*run parent loop till number of rows*/
 	{
-		for(k=1;k<i;k++)
+		for(k=1;k<i;k++)/*loop for printing initial space, before printing star*/
 		{
 		printf(" ");
 		}
-		for(j=5;j>=i;j--)
+		for(j=5;j>=i;j--)/*loop for printing star*/
 		{
 			printf("*");
 		}
