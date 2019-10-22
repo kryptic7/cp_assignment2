348da9de3a46a800a53859c809f6bd6bba1d61ce
kryptic7 <56120695+kryptic7@users.noreply.github.com>
2019-10-22 12:27:35 +0530

    Update q_3.c

--- q_3.c
+++ q_3.c
@@ -2,19 +2,19 @@
 void main()
 {
   int i,j,k;
-  for(i=0;i<=4;i++)
+  for(i=0;i<=4;i++)/*run parent loop till number of rows*/
   {
     for(j=0;j<=i;j++)
     {
-      if((i+j)%2==0)
+      if((i+j)%2==0)/*Conditional statement to check the condition and to print the right output*/
       {
-        printf("1");
+        printf("1");/*prints 1 if condition is true*/
       }
       else
       {
-        printf("0");
+        printf("0");/*prints 0 if condition is false*/
       }
     }
-    printf("\n");
+    printf("\n");/*prints a new line after the second loop ends*/
   }
 }
